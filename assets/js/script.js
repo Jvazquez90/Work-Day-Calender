d73fecec37ec127bebb20e9a460ac46cc3d98ade
jvazquez <87344928+Jvazquez90@users.noreply.github.com>
2021-09-26 14:17:13 -0400

    fixing repo

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+// Display Current Day, Date, Month and Year
+var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm a");
+$("#currentDay").html(currentDate);
+
+
+$(document).ready(function () {
+    // click listener for saveBtn
+    $(".saveBtn").on("click", function () {
+        // Get nearby values
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, value);
+    })
+   
+    function time() {
+        var currentTime = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for background indicators
+            if (blockTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    //Local storage
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    time();
+})
